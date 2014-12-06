cd91793409331635d7c955343aa44b7250fa37d0
SindhujaJeyabal <sindhuja.sj@gmail.com>
2014-12-06 02:06:14 -0800

    skeleton code with stop word removal, stemming, n grams, colocations, chunking, cross validation - data re-tokenization pending

--- Scripts/ted_object.py
+++ Scripts/ted_object.py
@@ -5,29 +5,31 @@ class ted(object):
 
 	def __init__(self):
 		pass
+	def dummy_function(self):
+		print "I am a dummy"
 
 	def words_talk(self):
-		return pickle.load('words_talks.p')
+		return pickle.load(open('../Corpus/words_talks.p', 'rb'))
 
 	def words_bag(self):
 		'''list of str'''						# got it!
-		return pickle.load('words_bag.p')
+		return pickle.load(open('../Corpus/words_bag.p', 'rb'))
 
 	def sents_talk(self):
 		'''list of (list of str)'''				# got it!
-		return pickle.load('sents_talks.p')
+		return pickle.load(open('../Corpus/sents_talks.p', 'rb'))
 
 	def sents_bag(self):
 		'''list of (list of str)'''				# got it!
-		return pickle.load('sents_bag.p')
+		return pickle.load(open('../Corpus/sents_bag.p', 'rb'))
 
 	def tagged_words_talk(self):
 		'''list of (str,str) tuple'''			# got it!
-		return pickle.load('tagged_words_talks.p')		
+		return pickle.load(open('../Corpus/tagged_words_talks.p', 'rb'))		
 
 	def tagged_sents_talk(self):
 		'''list of (list of (str,str))'''		# got it!
-		return pickle.load('tagged_sents_talks.p')
+		return pickle.load(open('../Corpus/tagged_sents_talks.p', 'rb'))
 
 	def tagged_paras(self):
 		'''list of (list of (list of (str,str)))'''
